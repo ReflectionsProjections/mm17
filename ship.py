a9dc7edadc86ad41c812c867ff26ec18bbac9156
Cole Gleason <cagleas2@illinois.edu>
2011-09-18 16:22:20 -0500

    Set up collision detection for ship.fire(), needs testing and code look over

--- ship.py
+++ ship.py
@@ -14,11 +14,16 @@ class Ship(object):
 
 	def __init__(self, player, position):
 		self.owner = player
-		self.id Game.next_id()
+		self.id = Game.next_id()
 		self.health = 100
-		self.max_thrust = 10
+		self.max_velocity = 10
+		self.max_accel = 2
 		self.position = position
 		self.velocity = (0,0)
+		self.events = []
+		self.scan_range = 2000
+		self.weapon_range = 1000
+		self.weapon_strength = 100
 
 	def step(self, dt):
 		vx, vy = self.velocity
@@ -28,6 +33,7 @@ class Ship(object):
 	def thrust(self, accel):
 		""" Must specify an angle. How does this tie into server? """
 		dx, dx = accel
+		# test to see if the accel is greater than allowed max
 		mag = sqrt(dx*dx + dy*dy)
 		if mag > self.max_accel:
 			scale = self.max_accel/mag
@@ -35,17 +41,50 @@ class Ship(object):
 			scale = 1
 		x, y = self.velocity
 		self.velocity = (x+scale*dx, y+scale*dy)
+		# scale doown to max velocity
+		vel_mag = sqrt(self.velocity[0]**2, self.velocity[1]**2)
+		vel_scale = max_velocity/vel_mag
+		self.velocity = vel_scale * self.velocity
+		
 
-	def fire(self, angle, map):
+	def fire(self, angle):
 		"""Fire at angle into the ships found in the map"""
-		pass
+		width = 10
+		length = self.weapon_strength
+		# rectangle containing laser beam.  Four points, counter clockwise
+		beam = (((width/2) * sin(angle), (width/2) * -cos(angle)), #side 1
+			((width/2) * sin(angle) + length * sin(angle),
+			 (width/2) * -cos(angle) + length * -cos(angle)),  #side 2
+			((width/2) * -sin(angle) + length * -sin(angle),
+			 (width/2) * cos(angle) + length * cos(angle)),    #side 3
+			((width/2) * -sin(angle), (width/2) * cos(angle))) #side 4
+
+		within_beam = [] # list for objects in beam
+		for obj in Game.game_map.objects:
+			if circle_in_rect(beam, obj.position, obj.radius):
+				within_beam.append(obj)
+		
+		if len(within_beam) == 0:
+			# No object hit
+			self.events.append(("shot", None))
+		else:
+			within_beam.sort(key=distance(self.position, obj.position))
+			# Hit first in line, record id
+			self.events.append(("shot", within_beam[0].id))
+			# register damage with hit object
+			dist = distance(self.position, within_beam[0].position)
+			damage_amt = self.weapon_strength *(self.weapon_range - \
+								    dist)/weapon_range 
+			within_beam[0].events(("damage", self.id, damage_amt)
+					   
 
 	def scan(self, ship):
 		"""Returns fuzzzed info if ship is in range, or None"""
+		# Multiply the returned distance by a random value
 		dist = distance(self.position, ship.position)
-		dist_error = random.uniform(0.5,1.5)
-		dist *= dist_error
 		if dist < self.scan_range:
+			dist_error = random.uniform(0.5,1.5)
+			dist *= dist_error
 			dx = ship.position[0] - self.position[0]
 			dy = ship.position[1] - self.position[1]
 			if dist == 0:
@@ -58,4 +97,5 @@ class Ship(object):
 					angle += 2*math.pi
 				elif angle > math.pi:
 					angle -= 2*math.pi
-			return angle, dist
+			return angle, dist, ship.health
+		else: return None
--- vector.py
+++ vector.py
@@ -1,3 +1,54 @@
 def distance(pos1, pos2):
 	"""Returns the distance between two (x, y) position tuples"""
 	return sqrt((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)
+
+def intersect_circle(center, radius, line):
+	""" Checks to see if a line intersects with a circle.
+	center = (x,y) coordinate of circle center
+	radius = radius of circle
+	line = ((x, y), (x,y)) start and end points of line
+
+	returns True/False"""
+
+	line_v = line[1][0]-line[0][0], line[0][1]-line[0][1]           # Vector of line
+        line_len = sqrt(line_v[0]**2 + line_v[1]**2)         # Length of line
+	line_v = line_v[0]/line_len, line_v[1]/line_len               # unit vector of line
+	normal = -line_v[1], line_v[0]                    # normal unit vector
+        center_line_v = center[0]-line[1][0], center[1]-line[1][1] # vector from point to end
+	dist = fabs(ac[0]*n[0]+ac[1]*n[1]) # Projection of center_line_v to n (the minimum distance)
+	if dist < radius: return True
+	else: return False
+
+
+def circle_in_rect(rect, center, radius):
+	""" Checks to see if a circle intersects the rectangle.
+	Also returns true if center is in rectangle.
+	rect should be a tuple of fours points, stored CCW
+	point should be an x,y coordinate
+	"""
+
+	# each side has a start point and a vector
+	sides = [(rect[0], (rect[1][0] - rect[0][0], rect[1][1] - rect[0][1])),
+		 (rect[1], (rect[2][0] - rect[1][0], rect[2][1] - rect[1][1])),
+		 (rect[2], (rect[3][0] - rect[2][0], rect[3][1] - rect[2][1])),
+		 (rect[3], (rect[0][0] - rect[3][0], rect[0][1] - rect[3][1]))]
+
+	center_right_of_side = 0 # must be four to be true
+	for side in sides:
+		# find vector bewtween point and start of side
+		vector = (side[0][0] - center[0], siode[0][1] - center[1])
+		# take cross product between vector and side
+		cross_prod = side[1][0]*vector[1] - side[1][1]*vector[0]
+		# if side intersects ciclre, return true
+		if intersect_circle(center, radius, side[1]):
+			return True
+		# if the following is true for all sides, center is in rect
+		if cross_prod > 0:
+			center_right_of_side++
+	# if we have 4, then circle is in rect
+	if center_right_of_side == 4:
+		return True
+	else:
+		return False
+
+
