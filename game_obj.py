42e3386f8758566081125f2baef49f9c5a43f0b9
Nelson Osacky <osacky1@sushi-k.acm.uiuc.edu>
2011-09-23 21:49:34 -0500

    added spaces and documentation for game_obj.py

--- game_obj.py
+++ game_obj.py
@@ -1,6 +1,8 @@
 class GameObject(object):
-	"""Base class for all game objects on the map since they need 
-	certain common info"""
+	"""Base class for all game objects on the map since they need
+	certain common info
+	"""
+	
 	def __init__(self, game, position, owner):
 		self.game = game
 		self.position = position
@@ -9,27 +11,41 @@ class GameObject(object):
 		self.size = 1
 		self.owner = owner
 		self.alive = True
+
 		# holds all events to be processed on turn handle
 		self.events = []
+		
 		# holds results from turns to be returned to user
 		# dict of lists accessed like results[turn]
 		self.results = {0: []}
+		
 		# set methods used to true in this dict to prevent
 		# double dipping
 		self.methods_used = {}
 
 	def step(self, dt):
-		"""Timestep executed every turn."""
+		"""Timestep executed every turn.
+		
+		@param dt: whatever dt means
+		"""
+		
 		vx, vy = self.velocity
 		x, y = self.position
 		self.position = (x + dt*vx, y + dt*vy)
 
 	def handle_damage(self, damage_event):
-		"""By default, do nothing."""
+		"""By default, do nothing.
+		
+		@param damage_event: whatever damage_events are
+		"""
+		
 		pass
 
 	def to_dict(self):
-		"""JSON serializable representation."""
+		"""Returns the current state in JSON serializable representation.
+		
+		"""
+		
 		state = {'obj_id': id(self),
 				'owner': self.owner,
 				'position':self.position,
