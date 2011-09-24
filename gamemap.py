d705a4568e8cc316512138449ae7215e2c93291a
Nelson Osacky <osacky1@illinois.edu>
2011-09-23 22:06:46 -0500

    fixed documentation on gamemap.py

--- gamemap.py
+++ gamemap.py
@@ -4,15 +4,24 @@ from ship import Ship
 from vector import distance
 
 class Map(object):
+	"""Map class that contains objects
 
+	"""
 	def __init__(self, maxPlayers):
+		"""Initializes the game map
+		
+		@param maxPlayers: The maximum number of players the map supports
+		"""
 		self.origin = (0, 0)
 		self.maxPlayers = maxPlayers
 		self.objects = {}
 		self.ships = {}
 
 	def add_object(self, object):
-		"""Adds an object to the map's global object dictionary."""
+		"""Adds an object to the map's global object dictionary.
+		
+		@param object: Object to be added to the map
+		"""
 		objID = id(object)
 		if objID not in self.objects.keys():
 			self.objects[objID] = object
@@ -21,6 +30,10 @@ class Map(object):
 		return objID
 
 	def radar(self, position, range):
-		"""Returns all objects within a certain radius of a position"""
+		"""Returns all objects within a certain radius of a position
+		
+		@param position: Position of the object to get radar
+		@param range: Range of the radar
+		"""
 		return [object for object in self.objects.itervalues()
 				if distance(position, object.position) <= range]
