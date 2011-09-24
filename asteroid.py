2348aa84d1e8caccae596d8aae39fe0babb56c0c
Drew Cross <mstngdrew212@gmail.com>
2011-09-24 01:52:03 -0500

    Added _to_dict to asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -8,10 +8,19 @@ class Asteroid(GameObject):
 		super(Asteroid, self).__init__(game, position)
 		self.size = size
 		self.resources = size * Constants.asteroid_scale
-
+		self.refinery = None
 
 	def pullResources():
 		self.resources -= Constants.resource_pull
 
 if __name__ == '__main__':
 	unittest.main()
+
+	def _to_dict(self):
+		state = {'type':'asteroid',
+				'id':id(self)
+				'position': self.position
+				'resources' : self.resources
+				'size' : self.size
+				'refinery' : id(self.refinery)}
+		return state
